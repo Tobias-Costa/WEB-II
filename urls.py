f7858bdbff699d41b67d738e3863c188ee00a8f6
Tobias Costa <126257040+Tobias-Costa@users.noreply.github.com>
2024-08-09 23:12:27 -0300

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('<str:pais_medalhas>', views.atletas_medalhas, name="atletas_medalhas"),
-    
-]
